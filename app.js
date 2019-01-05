78d79ed84968149acd9342aa45d543bd6d056bf7
James Turello <jturello@yahoo.com>
2019-01-05 05:20:26 -0500

    add route - get /api/genres

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ const app = express();
 app.use(express.json());
 
 
+const genres = [
+  { "id": "1", "name": "Drama" },
+  { "id": "2", "name": "Action" },
+  { "id": "3", "name": "Horror" },
+  { "id": "4", "name": "Comedy" },
+  { "id": "5", "name": "Science Fiction" }
+];
+
+app.get('/api/genres', (req, res) => {
+  console.log('in app.get(api/genre');
+  res.send(genres);
+});
+
+
+const port = process.env.PORT || 3003;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
