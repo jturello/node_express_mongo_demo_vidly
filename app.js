c2ce421d649cb1b668089b8b4a3e93a530351262
James Turello <jturello@yahoo.com>
2019-01-09 05:13:40 -0500

    add route handlers get '/' & get '/api/genres/:id'

--- app.js
+++ app.js
@@ -6,18 +6,27 @@ app.use(express.json());
 
 
 const genres = [
-  { "id": "1", "name": "Drama" },
-  { "id": "2", "name": "Action" },
-  { "id": "3", "name": "Horror" },
-  { "id": "4", "name": "Comedy" },
-  { "id": "5", "name": "Science Fiction" }
+  { "id": 1, "name": "Drama" },
+  { "id": 2, "name": "Action" },
+  { "id": 3, "name": "Horror" },
+  { "id": 4, "name": "Comedy" },
+  { "id": 5, "name": "Science Fiction" }
 ];
 
+app.get('/', (req, res) => { 
+  res.send('Welcome to Vidly!');
+});
+
 app.get('/api/genres', (req, res) => {
-  console.log('in app.get(api/genre');
   res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(400).send(`Genre with ID ${req.params.id} not found`);
+  
+  res.send(genre);
+});
 
 const port = process.env.PORT || 3003;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
