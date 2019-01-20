11afcc7a65015d6686ff4bd8ce6f8a8757526337
James Turello <jturello@yahoo.com>
2019-01-20 06:22:33 -0500

    move home route to module /routes/home

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const Joi = require('joi');
 const express  = require('express');
 const app = express();
 const genres = require('./routes/genres');
+const home = require('./routes/home');
 
 app.use(express.json());
 app.use('/api/genres', genres);
-
-app.get('/', (req, res) => { 
-  res.send('Welcome to Vidly!');
-});
+app.use('/', home);
 
 const port = process.env.PORT || 3003;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
