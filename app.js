680c90e8b37f7dc7c115d8a08c208d2a841695cc
James Turello <jturello@yahoo.com>
2019-01-20 06:08:46 -0500

    move genres route handlers to module /routes/genres

--- app.js
+++ app.js
@@ -1,71 +1,14 @@
 const Joi = require('joi');
 const express  = require('express');
 const app = express();
+const genres = require('./routes/genres');
 
 app.use(express.json());
-
-
-const genres = [
-  { "id": 1, "name": "Drama" },
-  { "id": 2, "name": "Action" },
-  { "id": 3, "name": "Horror" },
-  { "id": 4, "name": "Comedy" },
-  { "id": 5, "name": "Science Fiction" }
-];
+app.use('/api/genres', genres);
 
 app.get('/', (req, res) => { 
   res.send('Welcome to Vidly!');
 });
-
-app.get('/api/genres', (req, res) => {
-  res.send(genres);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id));
-  if (!genre) return res.status(400).send(`Genre with ID ${req.params.id} not found`);
-  
-  res.send(genre);
-});
- 
-app.post('/api/genres', (req, res) => {
-  
-  const { error } = validateGenre (req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  };
-
-  genres.push(genre);
-  res.status(201).send(genre);
-});
-
-app.put('/api/genres/:id', (req, res) => {
-  const genre = genres.find(o => o.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send(`Genre with ID ${req.params.id} not found`);
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-app.delete('/api/genres/:id', (req, res) => {
-  const genre = genres.find(o => o.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send(`Genre with ID ${req.params.id} not found`);
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send(genre);
-});
-
-function validateGenre(genre) {
-  const schema = { name: Joi.string().min(3).required() };
-  return Joi.validate(genre, schema);
-}
 
 const port = process.env.PORT || 3003;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
