5428cee10c2a4fceb65cd0c8580c54fc8002264d
James Turello <jturello@yahoo.com>
2019-01-24 10:58:42 -0500

    add static middleware and README file

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('views', './views'); // default
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
+app.use(express.static('public'));
 app.use(helmet());
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
---
+++ public/README.md
@@ -0,0 +1,5 @@
+# Node-Express-Mongo-Demo-Vidly
+
+> Movie rental demo app written in Express.js using MongoDB and Mongoose 
+
+
