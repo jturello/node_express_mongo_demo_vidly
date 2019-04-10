60a3ee4ae42dbd9180233d32a98d3a35f98bb289
James Turello <jturello@yahoo.com>
2019-04-10 18:32:41 -0400

    add get api/genre tests

--- spec/genre/genre_unit_spec.js
+++ spec/genre/genre_unit_spec.js
@@ -1,20 +1,27 @@
-// const app = require('../app.js'); 
 const request = require('request');
 const server = require('../../server');
 
-const base_url = "http://localhost:3000";
+describe("get api/genres", () => {
+  const endpoint = "http://localhost:3000/api/genres/";
 
-describe("genre_route", () => {
-  describe("get api/genres", function() {
-    it("returns 200 status", function(done) {
-      const endpoint = "http://localhost:3000/api/genres/";
-
-      request.get(endpoint, function(err, response) {
-	expect(response.statusCode).toEqual(200);
-	// console.log(response);
-	done();
-      });
+  it("returns 200 status", (done) => {
+    request.get(endpoint, function(err, res) {
+      expect(res.statusCode).toEqual(200);
+      done();
     });
   });
 
+  it("returns all (5) genres in the datastore", (done) => {
+    request.get(endpoint, function(err, res) {
+      expect(JSON.parse(res.body).length).toEqual(5);
+      done();
+    });
+  });
+
+  it("returns header content-type application/json; charset=utf-8", (done) => {
+    request.get(endpoint, function(err, res) {
+      expect(res.headers['content-type']).toEqual("application/json; charset=utf-8"); 
+      done();
+    });
+  });
 });
